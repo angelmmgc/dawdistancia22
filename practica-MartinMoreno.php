4f707843494049d2cc81606ed45f7c144c8beade
Angel <74067876+angelmmgc@users.noreply.github.com>
2022-04-25 20:39:02 +0200

    añadiendo archivo de practica

---
+++ practica-MartinMoreno.php
@@ -0,0 +1,62 @@
+<html>
+        <head>
+        </head>
+
+        <body>
+        <form id="form" method="post" action="practica-Martin.php">
+                nombre : <input type="text" name="nombre"/>
+                <br><br>
+                apellidos : <input type="text" name="apellido"/>
+                <br><br>
+                cual es tu modulo favorito : <input type="text" name="modulo"/>
+                <br><br>
+                <input type="submit" value="enviar"/>
+        </form>
+        </body>
+
+
+
+
+</html>
+
+<?php
+/*
+*funcion que muestra un saludo.
+*@author angel martin moreno
+*@version 0
+*@internal. esta funcion muestra un saludo con el nombre y el apellido de los c>
+*@param $nombre. string .guarda el nombre
+*@param $apellido. string.guarda el apellido.
+*@return type void.
+*/
+
+
+function saludo($nombre,$apellido){
+
+        return "<h1>HOLA MUNDO SOY  {$nombre} {$apellido}</h1>";
+}
+
+/*
+*funcion que muestra un texto.
+*@author
+*@version 0
+*@internal .muestra un texto con el modulo favorito introducido en el campo de >
+*@param $modulo.  string .guarda el modulo.
+*@return type void.
+*/
+
+function modulo($modulo){
+
+        return "<h2>MI MODULO FAVORITO ES {$modulo}</h2>";
+
+}
+
+//guardamos en las variables los datos de los campos de texto
+        $nombre= $_POST['nombre'];
+        $apellido=$_POST['apellido'];
+        $modulo = $_POST['modulo'];
+//ejecutamos las funciones
+        echo saludo($nombre,$apellido);
+        echo modulo($modulo);
+
+?>
